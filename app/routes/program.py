d4da02d6933a79fd8236b7bae49a6ae046a44442
Sandyfirdaus1 <shandyfirdaus99@gmail.com>
2025-01-18 21:32:10 +0700

    Initial commit

---
+++ app/routes/program.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, render_template, current_app, Blueprint, jsonify, redirect, url_for
+import jwt
+
+program_ = Blueprint('program', __name__)
+
+@program_.route('/program')
+def program():
+    myToken = request.cookies.get("mytoken")
+    SECRET_KEY = current_app.config['SECRET_KEY']
+    try:
+        payload = jwt.decode(myToken, SECRET_KEY, algorithms=["HS256"])
+        user_info = current_app.db.users.find_one({"username": payload["id"]})
+        return render_template('dashboard/program.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("auth.login"))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("auth.login"))
+
+@program_.route('/save-status-screening', methods=["POST"])
+def save_status_screening():
+    username = request.form['username']
+    scoreScreening = request.form['resultText']
+    doc = {
+        "username": username,
+        "scoreScreening": scoreScreening
+    }
+
+    exists = bool(current_app.db.status_screening.find_one({"username": username}))
+    if exists:
+        return jsonify({
+            "exists": exists,
+            "result": "Sudah Pernah Screening",
+        })
+    else:
+        current_app.db.status_screening.insert_one(doc)
+        return jsonify({
+            "exists": exists,
+            "result": "Success",
+        })
+
+@program_.route('/check-status-screening', methods=["POST"])
+def check_status_screening():
+    username = request.form['username']
+
+    exists = bool(current_app.db.status_screening.find_one({"username": username}))
+    if exists:
+        return jsonify({
+            "exists": exists,
+            "result": "Sudah Pernah Screening",
+        })
+    else:
+        return jsonify({
+            "exists": exists,
+            "result": "Belum Pernah Screening",
+        })
