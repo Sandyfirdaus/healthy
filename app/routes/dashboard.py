dffb94786335f16db7f29ea9393e771f37861236
Ardiansputrraa <ardiansaputra2580@gmail.com>
2025-01-30 12:55:02 +0700

    update

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,13 +1,24 @@
-from flask import Flask, request, render_template, current_app, Blueprint, jsonify, redirect, url_for, make_response
+from flask import Flask, Response, request, render_template, current_app, Blueprint, jsonify, redirect, url_for, make_response
 import hashlib
 from datetime import datetime, timedelta
 import jwt
+import io
 
 dashboard_ = Blueprint('dashboard', __name__)
 
 @dashboard_.route('/dashboard')
 def login_admin():
-    return render_template('dashboard_admin/dashboard.html')
+    myToken = request.cookies.get("mytoken")
+    SECRET_KEY = current_app.config['SECRET_KEY']
+    try:
+        payload = jwt.decode(myToken, SECRET_KEY, algorithms=["HS256"])
+        user_info = current_app.db.users.find_one({"username": payload["id"]})
+        return render_template('dashboard_admin/dashboard.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("auth_admin.login_admin"))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("auth_admin.login_admin"))
+    
 
 @dashboard_.route('/get-data', methods=['GET'])
 def get_data():
@@ -27,3 +38,39 @@ def delete_materi():
     materi = request.form['materi']
     current_app.db.status.delete_one({'username': username, 'materi': materi})
     return jsonify({'msg': 'Data materi berhasil dihapus!'})
+
+# @dashboard_.route("/download-pdf")
+# def download_pdf():
+#     # Ambil data dari MongoDB
+#     data = collection.find()
+
+#     # Buat buffer untuk menyimpan file PDF di memori
+#     buffer = io.BytesIO()
+#     pdf = canvas.Canvas(buffer, pagesize=letter)
+#     width, height = letter
+
+#     # Judul PDF
+#     pdf.setFont("Helvetica-Bold", 14)
+#     pdf.drawString(200, height - 50, "Laporan Data MongoDB")
+
+#     # Mulai menulis data dari MongoDB ke PDF
+#     y_position = height - 100
+#     pdf.setFont("Helvetica", 12)
+
+#     for doc in data:
+#         line = f"ID: {doc['_id']}, Nama: {doc.get('nama', 'N/A')}, Email: {doc.get('email', 'N/A')}"
+#         pdf.drawString(50, y_position, line)
+#         y_position -= 20  # Pindah ke baris berikutnya
+
+#         if y_position < 50:  # Jika halaman hampir habis, buat halaman baru
+#             pdf.showPage()
+#             pdf.setFont("Helvetica", 12)
+#             y_position = height - 50
+
+#     pdf.save()
+    
+#     buffer.seek(0)
+    
+#     # Mengembalikan file PDF sebagai response
+#     return Response(buffer, mimetype="application/pdf", headers={"Content-Disposition": "attachment;filename=laporan.pdf"})
+
