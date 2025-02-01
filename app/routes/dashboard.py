ad8c2273d266f395d4b83b66853c36742ec21e27
Sandyfirdaus1 <shandyfirdaus99@gmail.com>
2025-02-02 00:45:02 +0700

    update\

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -38,39 +38,3 @@ def delete_materi():
     materi = request.form['materi']
     current_app.db.status.delete_one({'username': username, 'materi': materi})
     return jsonify({'msg': 'Data materi berhasil dihapus!'})
-
-# @dashboard_.route("/download-pdf")
-# def download_pdf():
-#     # Ambil data dari MongoDB
-#     data = collection.find()
-
-#     # Buat buffer untuk menyimpan file PDF di memori
-#     buffer = io.BytesIO()
-#     pdf = canvas.Canvas(buffer, pagesize=letter)
-#     width, height = letter
-
-#     # Judul PDF
-#     pdf.setFont("Helvetica-Bold", 14)
-#     pdf.drawString(200, height - 50, "Laporan Data MongoDB")
-
-#     # Mulai menulis data dari MongoDB ke PDF
-#     y_position = height - 100
-#     pdf.setFont("Helvetica", 12)
-
-#     for doc in data:
-#         line = f"ID: {doc['_id']}, Nama: {doc.get('nama', 'N/A')}, Email: {doc.get('email', 'N/A')}"
-#         pdf.drawString(50, y_position, line)
-#         y_position -= 20  # Pindah ke baris berikutnya
-
-#         if y_position < 50:  # Jika halaman hampir habis, buat halaman baru
-#             pdf.showPage()
-#             pdf.setFont("Helvetica", 12)
-#             y_position = height - 50
-
-#     pdf.save()
-    
-#     buffer.seek(0)
-    
-#     # Mengembalikan file PDF sebagai response
-#     return Response(buffer, mimetype="application/pdf", headers={"Content-Disposition": "attachment;filename=laporan.pdf"})
-
