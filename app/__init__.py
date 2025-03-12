60ffcba31847c5bd16c4c7d9165890beeb63a727
Sandyfirdaus1 <shandyfirdaus99@gmail.com>
2025-03-12 14:33:21 +0700

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -7,16 +7,17 @@ def create_app():
     app.config.from_object(Config)
     
     # Initialize MongoDB
-    client = MongoClient(app.config['MONGODB_URI'])
-    app.db = client[app.config['DBNAME']]
+    client = MongoClient(app.config["MONGODB_URI"])
+    app.db = client[app.config["DBNAME"]]
     
-    # Import and register Blueprints
+    # Import Blueprints
     from .routes import (
         home, artikeldepan, artikeldepan_details, homesignin, auth, auth_admin,
-        dashboard, program, program_content, program_details, artikel,
-        artikel_details, profile
+        dashboard, add_articles, add_program, program, program_content, 
+        program_details, artikel, artikel_details, profile
     )
     
+    # List of Blueprints
     blueprints = [
         home.home_,
         artikeldepan.artikeldepan_,
@@ -25,6 +26,8 @@ def create_app():
         auth.auth_,
         auth_admin.auth_admin_,
         dashboard.dashboard_,
+        add_articles.add_articles_,
+        add_program.add_program_,
         program.program_,
         program_content.program_content_,
         program_details.program_details_,
@@ -33,6 +36,7 @@ def create_app():
         profile.profile_
     ]
     
+    # Register Blueprints
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
     
---
+++ app/routes/add_program.py
@@ -0,0 +1,36 @@
+from flask import Flask, Response, request, render_template, current_app, Blueprint, jsonify, redirect, url_for, make_response
+import hashlib
+from datetime import datetime, timedelta
+import jwt
+import io
+
+add_program_ = Blueprint('add_program', __name__)
+
+@add_program_.route('/dashboard')
+def login_admin():
+    myToken = request.cookies.get("mytoken")
+    SECRET_KEY = current_app.config['SECRET_KEY']
+    try:
+        # Decode JWT token
+        payload = jwt.decode(myToken, SECRET_KEY, algorithms=["HS256"])
+        
+        # Cari user info berdasarkan username yang ada di payload
+        user_info = current_app.db.users_admin.find_one({"username": payload["id"]})
+
+        # Cek jika user_info ada dan username-nya sesuai dengan 'admin.psidamai'
+        if user_info and user_info.get('username') == 'admin.psidamai':
+            return render_template('dashboard_admin/dashboard.html', user_info=user_info)
+        else:
+            # Jika username bukan admin.psidamai, redirect ke halaman login
+            return redirect(url_for("auth_admin.login_admin"))
+        
+    except jwt.ExpiredSignatureError:
+        # Token sudah expired, redirect ke halaman login
+        return redirect(url_for("auth_admin.login_admin"))
+    except jwt.exceptions.DecodeError:
+        # Jika token tidak valid
+        return redirect(url_for("auth_admin.login_admin"))
+    
+@add_program_.route('/add_program')
+def add_program():
+    return render_template('dashboard_admin/add_program.html')
